4d7899277d3d09b325d350a71b71432386a14a8a
Sam Boyer <tech@samboyer.org>
2014-01-31 08:52:32 -0500

    Add gitrd.go with the most rudimentary args & usage.

---
+++ gitrd.go
@@ -0,0 +1,36 @@
+/*
+gitrd is a daemon to serve all your remote git needs.
+
+It handles SSH, (Smart) HTTP, and at some point, maybe even the git protocol,
+all through a single daemon. It also exposes a RESTful interface for accessing
+data about the git repository that is not available with conventional git.
+
+By combining all these behaviors into a single daemon, it simplifies the task
+of running a large git hosting service. gitrd handles routing, ident, auth
+and other configurable components to be managed through a single stack,
+eliminating the need to teach multiple different daemons about how your git
+infrastructure works.
+*/
+package gitrd
+
+import (
+	"github.com/jessevdk/go-flags"
+)
+
+type args struct {
+	Verbose bool `short:"v" long:"verbose" description:"enables verbose output"`
+	Quiet   bool `short:"q" long:"quiet" description:"turns off all output"`
+	// TODO figure out how to handle/reconcile a config dir with go-flags options.
+}
+
+var defaultArgs = &args{
+	Verbose: false,
+	Quiet:   false,
+}
+
+func main() {
+	p := flags.NewParser(defaultArgs, flags.HelpFlag|flags.PrintErrors)
+	p.Usage = `[OPTIONS] ...
+
+	gitrd is an all-in-one git daemon.`
+}
